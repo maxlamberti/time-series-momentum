02bca3a01818d29a38e370cc94e7d3a91b32d83f
maxlamberti <maximilien.lamberti@gmail.com>
2020-03-17 23:40:19 -0700

    adds data handling functions

---
+++ utils/data_handling.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def merge_asset_data(asset_to_df_map, create_time_asset_index):
+    "Merges multiple asset dataframes together."
+
+    asset_dfs = [df for asset, df in asset_to_df_map.items()]
+    combined_assets = pd.concat(asset_dfs, ignore_index=True)
+    combined_assets.set_index(['Asset', 'Date'], inplace=True, drop=False)
+    combined_assets.sort_index(inplace=True)
+    combined_assets['Asset_Col'] = combined_assets['Asset']
+    combined_assets['Date_Col'] = combined_assets['Date']
+    del combined_assets['Date']
+    del combined_assets['Asset']
+
+    return combined_assets
+
+
+def split_by_date(df, date_breakpoints):
+
+    data_sets = []
+    for idx, start in enumerate(date_breakpoints):
+        if idx == len(date_breakpoints) - 1:
+            break
+        end = date_breakpoints[idx + 1]
+        data_sets.append(df[(start <= df['Date_Col']) & (df['Date_Col'] < end)])
+
+    return data_sets
