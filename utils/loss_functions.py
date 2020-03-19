9cf71019bc736b6fb8516a1e9f2c2c9af1e5a1ba
maxlamberti <maximilien.lamberti@gmail.com>
2020-03-19 16:18:19 -0700

    updates loss functions

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import keras.backend as K
 
 
 def sharpe_loss(target_data, size_prediction, sigma_tgt=0.15):
@@ -9,10 +10,10 @@ def sharpe_loss(target_data, size_prediction, sigma_tgt=0.15):
     sigma_t = target_data[:, 1]
 
     # calc sharpe
-    ret = size_prediction * sigma_tgt * next_period_return / sigma_t
-    mean_ret = np.mean(ret)
-    mean_ret2 = np.mean(ret ** 2)
-    sharpe = mean_ret / np.sqrt(mean_ret2 - (mean_ret ** 2))
+    ret = size_prediction * sigma_tgt * next_period_return / (np.sqrt(252) * sigma_t)
+    mean_ret = K.mean(ret, axis=-1)
+    mean_ret2 = K.mean(ret ** 2, axis=-1)
+    sharpe = mean_ret / K.sqrt(mean_ret2 - (mean_ret ** 2))
 
     return -sharpe
 
@@ -25,7 +26,38 @@ def return_loss(target_data, size_prediction, sigma_tgt=0.15):
     sigma_t = target_data[:, 1]
 
     # calc mean_ret
-    ret = size_prediction * sigma_tgt * next_period_return / sigma_t
+    ret = size_prediction * sigma_tgt * next_period_return / (np.sqrt(252) * sigma_t)
+    mean_ret = K.mean(ret)
+
+    return -mean_ret
+
+
+def sharpe_loss_dummy(target_data, size_prediction, sigma_tgt=0.15):
+    """Sharpe ratio inspired loss function."""
+
+    # load parameters
+    next_period_return = target_data[:, 0]
+    sigma_t = target_data[:, 1]
+
+    # calc sharpe
+    ret = size_prediction * sigma_tgt * next_period_return / (np.sqrt(252) * sigma_t)
     mean_ret = np.mean(ret)
+    mean_ret2 = np.mean(ret ** 2)
+    sharpe = ((1 + mean_ret)**252 - 1) / np.sqrt(252 * (mean_ret2 - (mean_ret ** 2)))
+
+    return -sharpe
+
+
+def return_loss_dummy(target_data, size_prediction, sigma_tgt=0.15):
+    """Loss function driven by mean returns."""
+
+    # load parameters
+    next_period_return = target_data[:, 0]
+    sigma_t = target_data[:, 1]
+
+    # calc mean_ret
+    ret = size_prediction * sigma_tgt * next_period_return / (np.sqrt(252) * sigma_t)
+    mean_ret = np.mean(ret)
+    mean_ret = ((1 + mean_ret) ** 252 - 1)
 
     return -mean_ret
