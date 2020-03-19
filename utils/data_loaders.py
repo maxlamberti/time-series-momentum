4ad8c16f7a39a7caabc586200effda74a044ffb2
maxlamberti <maximilien.lamberti@gmail.com>
2020-03-19 16:13:58 -0700

    enables loading of fed funds rate data

--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -3,8 +3,13 @@ import numpy as np
 import pandas as pd
 
 
-def load_clc_db_records(path):
+def load_clc_db_records(path, feds_data=None):
     """Load all CLC futures data into dict. One entry per asset. Path specifies location of .csv files"""
+
+    if feds_data is not None:
+        short_rate = pd.read_csv(feds_data)
+        short_rate['DATE'] = pd.to_datetime(short_rate['DATE'], format='%Y-%m-%d')
+        feds_label = short_rate.columns[-1]
 
     data = {}
     files = [file for file in os.listdir(path) if '.csv' in file.lower()]
@@ -12,11 +17,19 @@ def load_clc_db_records(path):
         asset = file[:-4].split('_')[0]
         data[asset] = pd.read_csv(os.path.join(path, file), names=['Date', 'Open', 'High', 'Low', 'Settle', 'Volume', 'Open_Interest'])
         data[asset]['Date'] = pd.to_datetime(data[asset]['Date'], format='%m/%d/%Y')
+        if feds_data is not None:
+            data[asset] = data[asset].merge(short_rate, how='left', left_on='Date', right_on='DATE')
+            data[asset][feds_label].ffill(inplace=True)
+            data[asset][feds_label] /= 100
+            data[asset]['Short_Rate_Daily'] = (1 + data[asset][feds_label]) ** (1 / 252) - 1
+            data[asset]['Short_Rate_Annual'] = data[asset][feds_label]
+            del data[asset][feds_label]
+            del data[asset]['DATE']
 
     return data
 
 
 if __name__ == '__main__':
 
-    clc_data = load_clc_db('../data/clc/rev/')
+    clc_data = load_clc_db_records('../data/clc/rev/', '../data/DFF.csv')
     print(list(clc_data.values())[-1])
