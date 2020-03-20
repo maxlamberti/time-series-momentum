c10cd5349eaaf26d73970f52c251c11e83a0b3e6
maxlamberti <maximilien.lamberti@gmail.com>
2020-03-20 16:06:45 -0700

    changes datetime index creation to being optional

--- utils/data_handling.py
+++ utils/data_handling.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
 
-def merge_asset_data(asset_to_df_map, create_time_asset_index):
+def merge_asset_data(asset_to_df_map, create_time_asset_index=True):
     "Merges multiple asset dataframes together."
 
     asset_dfs = [df for asset, df in asset_to_df_map.items()]
     combined_assets = pd.concat(asset_dfs, ignore_index=True)
-    combined_assets.set_index(['Asset', 'Date'], inplace=True, drop=False)
-    combined_assets.sort_index(inplace=True)
-    combined_assets['Asset_Col'] = combined_assets['Asset']
-    combined_assets['Date_Col'] = combined_assets['Date']
-    del combined_assets['Date']
-    del combined_assets['Asset']
+
+    if create_time_asset_index:
+        combined_assets.set_index(['Asset', 'Date'], inplace=True, drop=False)
+        combined_assets.sort_index(inplace=True)
+        combined_assets['Asset_Col'] = combined_assets['Asset']
+        combined_assets['Date_Col'] = combined_assets['Date']
+        del combined_assets['Date']
+        del combined_assets['Asset']
 
     return combined_assets
 
