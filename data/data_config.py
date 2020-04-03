d2b53961c063430b218b52d67b0623d8887f5c25
maxlamberti <maximilien.lamberti@gmail.com>
2020-04-02 22:11:10 -0500

    adds a data config file with assets to be used for analysis

---
+++ data/data_config.py
@@ -0,0 +1,9 @@
+
+
+ASSETS_TO_USE = [
+	'AN', 'BN', 'CC', 'CN', 'CT', 'DT', 'DX', 'FB', 'FN', 'FX', 'GI',
+	'GS', 'JN', 'JO', 'KC', 'KW', 'LB', 'LX', 'MD', 'MW', 'NK', 'NR',
+	'PA', 'SB', 'SN', 'SP', 'TY', 'US', 'W', 'ZA', 'ZB', 'ZC', 'ZF',
+	'ZG', 'ZH', 'ZI', 'ZK', 'ZL', 'ZM', 'ZN', 'ZO', 'ZP', 'ZR', 'ZS',
+	'ZT', 'ZU', 'ZW', 'ZZ'
+]
